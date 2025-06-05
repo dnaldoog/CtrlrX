338c894a42d87dfbcc6e932b2ff3fd2fe6f3653a
Sgorpi <sgorpi@gmail.com>
2025-06-05 06:01:51 +0200

    Small CI improvements

--- Scripts/git-revision.sh
+++ Scripts/git-revision.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 projecttag=`git describe --tags --abbrev=0`
-revisioncount=`git log --oneline "$projecttag".. | wc -l | tr -d ' '`
-echo "$projecttag.$revisioncount"
+if [ -z "$projecttag" ]; then
+    projecttag="v0.0.1"
+    git rev-parse --short HEAD
+else
+    revisioncount=`git log --oneline "$projecttag".. | wc -l | tr -d ' '`
+    echo "$projecttag.$revisioncount"
+fi
