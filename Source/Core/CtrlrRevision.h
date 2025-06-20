88d46a67ac11c4fdccfe5bc2918572bac68befe0
Damien S <55164385+damiensellier@users.noreply.github.com>
2025-06-20 02:22:23 -0400

    5.6.34 UPDATE gitignore list and IDE files required for CI/CD workflow

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -6,7 +6,7 @@ static const char *ctrlrRevision          = JucePlugin_VersionString;
 #ifdef JUCE_MAC
 // macOS build: Use build script-generated timestamp
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2025-06-10 23:01:30"
+#define BUILD_TIMESTAMP "2025-06-19 17:30:11"
 #endif
 static const char *ctrlrRevisionDate      = BUILD_TIMESTAMP; // Updated v5.6.32. FIX for Xcode not updating build time properly and keeping the first build timestamp as ref
 
@@ -24,7 +24,7 @@ static const char *ctrlrRevisionDate      = __TIMESTAMP__;
 //headerFile="../../Source/Core/CtrlrRevision.h"
 //# Use sed to replace or append the BUILD_TIMESTAMP definition
 //if grep -q "BUILD_TIMESTAMP" "$headerFile"; then
-//    sed -i "" "s/#define BUILD_TIMESTAMP "2025-06-10 23:01:30"
+//    sed -i "" "s/#define BUILD_TIMESTAMP "2025-06-19 17:30:11"
 //else
-//    echo "#define BUILD_TIMESTAMP "2025-06-10 23:01:30"
+//    echo "#define BUILD_TIMESTAMP "2025-06-19 17:30:11"
 //fi
