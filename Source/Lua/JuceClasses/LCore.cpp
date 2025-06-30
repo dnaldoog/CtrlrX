6e5c6fdafd39738be053425c4b51af2a1046ab1e
Damien S <55164385+damiensellier@users.noreply.github.com>
2025-06-30 13:52:49 -0400

    5.6.34 UPDATE LuaBind InputStream class.
    *UPDATED LuaBind InputStream class. `LCore.cpp`
    https://github.com/damiensellier/CtrlrX/pull/116

--- Source/Lua/JuceClasses/LCore.cpp
+++ Source/Lua/JuceClasses/LCore.cpp
@@ -1085,12 +1085,18 @@ void LZipFile::wrapForLua(lua_State* L) // Updated v5.6.34. Thanks to @dnaldoog
 }
 
 
-void LInputStream::wrapForLua (lua_State *L)
+void LInputStream::wrapForLua (lua_State *L) // Updated v5.6.34. Thanks to @dnaldoog. https://github.com/damiensellier/CtrlrX/pull/116
 {
 	using namespace luabind;
 
 	module(L)
-    [
-		class_<InputStream>("InputStream")
-	];
+        [
+            class_<InputStream>("InputStream")
+                .def("readEntireStreamAsString", &InputStream::readEntireStreamAsString)
+                .def("readIntoMemoryBlock", (size_t(InputStream::*)(MemoryBlock&, ssize_t)) & InputStream::readIntoMemoryBlock)
+                .def("getTotalLength", &InputStream::getTotalLength)
+                .def("getPosition", &InputStream::getPosition)
+                .def("setPosition", &InputStream::setPosition)
+                .def("isExhausted", &InputStream::isExhausted)
+        ];
 }
