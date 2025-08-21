10e8997ad518dad0a369c3827213c4682fd12611
Damien S <55164385+damiensellier@users.noreply.github.com>
2025-08-20 21:56:33 -0400

    5.6.34 UPDATED CtrlrPropertyComponent Separated colourPicker from textButton
    SEE : https://github.com/damiensellier/CtrlrX/pull/165

--- Source/Core/CtrlrRevision.h
+++ Source/Core/CtrlrRevision.h
@@ -6,7 +6,7 @@ static const char *ctrlrRevision          = JucePlugin_VersionString;
 #ifdef JUCE_MAC
 // macOS build: Use build script-generated timestamp
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2025-08-19 22:43:25"
+#define BUILD_TIMESTAMP "2025-08-20 21:50:42"
 #endif
 static const char *ctrlrRevisionDate      = BUILD_TIMESTAMP; // Updated v5.6.32. FIX for Xcode not updating build time properly and keeping the first build timestamp as ref
 
@@ -24,7 +24,7 @@ static const char *ctrlrRevisionDate      = __TIMESTAMP__;
 //headerFile="../../Source/Core/CtrlrRevision.h"
 //# Use sed to replace or append the BUILD_TIMESTAMP definition
 //if grep -q "BUILD_TIMESTAMP" "$headerFile"; then
-//    sed -i "" "s/#define BUILD_TIMESTAMP "2025-08-19 22:43:25"
+//    sed -i "" "s/#define BUILD_TIMESTAMP "2025-08-20 21:50:42"
 //else
-//    echo "#define BUILD_TIMESTAMP "2025-08-19 22:43:25"
+//    echo "#define BUILD_TIMESTAMP "2025-08-20 21:50:42"
 //fi
