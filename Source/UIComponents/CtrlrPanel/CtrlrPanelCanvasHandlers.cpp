67485d62eb2bde61ab9fcdc99e4d0645abc293df
John Goodland <76831683+dnaldoog@users.noreply.github.com>
2026-07-22 19:32:30 +0900

    Changed wording less verbose 'Distribute vertically' 'Distribute horizontally'
    [ci skip]

--- Source/UIComponents/CtrlrPanel/CtrlrPanelCanvasHandlers.cpp
+++ Source/UIComponents/CtrlrPanel/CtrlrPanelCanvasHandlers.cpp
@@ -299,8 +299,8 @@ void CtrlrPanelCanvas::getEditMenu(PopupMenu &m)
 		m.addItem (AlignToBottom, "Align to bottom", true, false);
 		m.addItem (AlignToLeft, "Align to left", true, false);
 		m.addItem (AlignToRight, "Align to right", true, false);
-		m.addItem (DistributeHorizontally, "Distribute centres equidistantly horizontally", true, false);
-		m.addItem (DistributeVertically, "Distribute centres equidistantly vertically", true, false);
+		m.addItem (DistributeHorizontally, "Distribute horizontally", true, false);
+		m.addItem (DistributeVertically, "Distribute vertically", true, false);
 		m.addSeparator();
 		m.addItem(MatchWidth, "Match width to first selected", true, false);
 		m.addItem(MatchHeight, "Match height to first selected", true, false);
