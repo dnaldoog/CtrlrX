cb4615935c21d486e8f073ae86d51e0ac6eb30b4
John Goodland <76831683+dnaldoog@users.noreply.github.com>
2026-08-10 18:54:06 +0900

    Changed name of program 8 BETA

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -77,13 +77,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  1
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                8.0.12
+ #define JucePlugin_Version                8 BETA
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x8000c
+ #define JucePlugin_VersionCode            0x80000
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "8.0.12"
+ #define JucePlugin_VersionString          "8 BETA"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
