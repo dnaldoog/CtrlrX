c3dc184ed2e1c4876a22df4da23877b75edf1aee
John Goodland <76831683+dnaldoog@users.noreply.github.com>
2026-08-20 17:11:12 +0900

    Removed new function setFileHandler as redundant [ci skip]

--- Source/Lua/CtrlrLuaManager.cpp
+++ Source/Lua/CtrlrLuaManager.cpp
@@ -291,7 +291,6 @@ void CtrlrPanelCanvas::wrapForLua(lua_State *L) {
 				  .def("getLayerFromArray", &CtrlrPanelCanvas::getLayerFromArray)
 				  .def("getNumLayers", &CtrlrPanelCanvas::getNumLayers)
 				  .def("getLayerName", &CtrlrPanelCanvas::getLayerName)
-				  .def("setFileDropHandler", &CtrlrPanelCanvas::setFileDropHandler)
 				  .def("setCustomLookAndFeel",
 					   (void (CtrlrPanelCanvas::*)(const luabind::object &))&CtrlrPanelCanvas::setCustomLookAndFeel)];
 }
@@ -1277,7 +1276,6 @@ void CtrlrPanel::wrapForLua(lua_State *L) {
 			 .def("getFixedSlider", &CtrlrPanel::getFixedSliderComponent)
 			 .def("getImageSliderComponent", &CtrlrPanel::getImageSliderComponent)
 			 .def("getImageSlider", &CtrlrPanel::getImageSliderComponent)
-			 //.def("setFileDropHandler", &CtrlrPanelCanvas::setFileDropHandler)
 			 .def("getModulatorWithProperty",
 				  (CtrlrModulator * (CtrlrPanel::*)(const String &, const int)) & CtrlrPanel::getModulatorWithProperty)
 			 .def("getModulatorWithProperty", (CtrlrModulator * (CtrlrPanel::*)(const String &, const String &)) &
