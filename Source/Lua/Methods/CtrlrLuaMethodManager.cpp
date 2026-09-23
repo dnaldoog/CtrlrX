4e86380d97b873c6f6d5b27ee7f071cc99ac217f
John Goodland <76831683+dnaldoog@users.noreply.github.com>
2026-09-23 22:29:53 +0900

    pre merge

--- Source/Lua/Methods/CtrlrLuaMethodManager.cpp
+++ Source/Lua/Methods/CtrlrLuaMethodManager.cpp
@@ -360,46 +360,14 @@ const String CtrlrLuaMethodManager::getTemplateForProperty(const String &methodN
 	return ("");
 }
 
-// const StringArray CtrlrLuaMethodManager::getMethodList() {
-// 	StringArray ret;
-
-// 	for (int i = 0; i < methods.size(); i++) {
-// 		ret.add(methods[i]->getName());
-// 		DBG("ret ---" + String(methods[i]->getLeakedObjectClassName()));
-// 	}
-
-// 	// Sort method names naturally / alphabetically
-// 	ret.sortNatural();
-
-// 	return (ret);
-// }
-
 const StringArray CtrlrLuaMethodManager::getMethodList() {
 	StringArray ret;
 
 	for (int i = 0; i < methods.size(); i++) {
-		if (methods[i] == nullptr)
-			continue;
-
-		String name = methods[i]->getName();
-		String code = methods[i]->getCode();
-
-		// Skip tables entirely to keep the callback list clean
-		if (code.contains("{") && !code.contains("function")) {
-			continue;
-		}
-
-		String prefix = "[User] "; // Default: User/Generic function
-
-		if (code.contains("--[[ CtrlrModulator --]]") || code.contains("source")) {
-			prefix = "[Clbk] "; // Component/Modulator Callback
-		} else if (code.contains("--[[ MouseEvent --]]") || code.contains("event")) {
-			prefix = "[Evnt] "; // UI Mouse Event
-		}
-
-		ret.add(prefix + name);
+		ret.add(methods[i]->getName());
 	}
 
+	// Sort method names naturally / alphabetically
 	ret.sortNatural();
 
 	return (ret);
